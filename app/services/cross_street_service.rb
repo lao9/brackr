6b5744bc47b13617e8b6bb086d78f15345a4b32d
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-25 14:29:30 -0600

    finish cross street service and testing
    [Finishes #149522911]

---
+++ app/services/cross_street_service.rb
@@ -0,0 +1,24 @@
+class CrossStreetService
+
+  def initialize(coordinates)
+    @conn = Faraday.new("https://maps.googleapis.com")
+    @search_params = {
+      latlng: "#{coordinates[:lat]},#{coordinates[:long]}",
+      key: ENV['GOOGLE_MAP_KEY']
+    }
+  end
+
+  def find_by_coordinates
+    response = conn.get("/maps/api/geocode/json", search_params)
+    JSON.parse(response.body, symbolize_names: true)[:results][1][:address_components][0][:long_name]
+  end
+
+  def self.find_by_coordinates(coordinates)
+    service = CrossStreetService.new(coordinates)
+    service.find_by_coordinates
+  end
+
+  private
+    attr_reader :search_params, :conn
+
+end
