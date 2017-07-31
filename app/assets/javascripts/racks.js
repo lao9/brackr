4c3a23910d1599d301e9b2d5a3a95aa6735db70d
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-31 13:44:31 -0600

    add functionality for selecting particular brack

--- app/assets/javascripts/racks.js
+++ app/assets/javascripts/racks.js
@@ -4,20 +4,31 @@ $(document).ready(function(){
 
 })
 
+function toggleRackIndex(){
+  $('.racks-show').slideToggle(1000)
+  $('.racks-hide').slideToggle(1000)
+}
+
 function expandRackOptions() {
   $('.racks-index').on('click', function(event){
+    if ($(event.target).hasClass("rack")) {
+      var brackId = $(event.target).data("id")
+      refocusMap(brackId)
+    }
     if ($(this).hasClass("racks-hide")) {
-      $('.racks-show').slideToggle(1000)
-      $('.racks-hide').slideToggle(1000)
+      toggleRackIndex()
       $("#map").animate({
         height: "-=120",
       }, 1000)
     } else {
-      $('.racks-hide').slideToggle(1000)
-      $('.racks-show').slideToggle(1000)
+      toggleRackIndex()
       $("#map").animate({
         height: "+=120"
       }, 1000)
     }
   })
+}
+
+function refocusMap(brackId) {
+  console.log(`Brack ID ${brackId}`)
 }
