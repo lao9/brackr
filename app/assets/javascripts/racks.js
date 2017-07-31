f9bd5b21e9d54ec14b4500ed0c5fcb835c96c71f
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-31 11:59:41 -0600

    add show and hide nearest toggle

--- app/assets/javascripts/racks.js
+++ app/assets/javascripts/racks.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
 
   expandRackOptions()
-  
+
 })
 
 function expandRackOptions() {
-  $('.racks-index').on('click', function(){
-    console.log("Here!")
+  $('.racks-index').on('click', function(event){
+    if ($(this).hasClass("racks-hide")) {
+      $('.racks-show').slideToggle(1000)
+      $('.racks-hide').slideToggle(1000)
+      $("#map").animate({
+        height: "-=200",
+      }, 1000)
+    } else {
+      $('.racks-hide').slideToggle(1000)
+      $('.racks-show').slideToggle(1000)
+      $("#map").animate({
+        height: "+=200"
+      }, 1000)
+    }
   })
 }
