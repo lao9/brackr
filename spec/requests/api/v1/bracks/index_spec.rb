8313b407a7430bdc4fa3b57dd955f0cd00c41a5c
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-07-31 17:07:26 -0600

    add cross streets to API endpoints

--- spec/requests/api/v1/bracks/index_spec.rb
+++ spec/requests/api/v1/bracks/index_spec.rb
@@ -29,6 +29,7 @@ describe "Bracks API" do
       expect(first_brack).to have_key(:lat)
       expect(first_brack).to have_key(:long)
       expect(first_brack).to have_key(:distance)
+      expect(first_brack).to have_key(:cross_streets)
 
       expect(first_brack[:id]).to eq(closest.id)
       expect(last_brack[:id]).to eq(furthest.id)
@@ -55,6 +56,7 @@ describe "Bracks API" do
       expect(first_brack).to have_key(:lat)
       expect(first_brack).to have_key(:long)
       expect(first_brack).to have_key(:distance)
+      expect(first_brack).to have_key(:cross_streets)
 
       expect(first_brack[:id]).to eq(closest.id)
       expect(last_brack[:id]).to eq(furthest.id)
