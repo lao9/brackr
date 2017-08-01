1307f8c88a48f094e2c9cf1927c9aa5815b9a36c
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-08-01 07:12:26 -0600

    might have effed up my map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -16,6 +16,57 @@ BrackMap.drawMap = function(coords) {
     zoom: 18,
     center: brackMap.center
   });
+  // create search box and link it to the UI element
+  // var input = document.getElementById('pac-input')
+  // var searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // map.addListener('bounds_changed', function(){
+  //   searchBox.setBounds(map.getBounds());
+  // })
+  // searchBox.addListener('places_changed', function(){
+  //   var places = searchBox.getPlaces();
+  //   if (places.length == 0) {
+  //     return;
+  //   }
+  //   // clear out the old markers
+  //   brackMarkers.forEach(function(marker) {
+  //     marker.setMap(null);
+  //   });
+  //   brackMarkers = [];
+  //
+  //   // For each place, get the icon, name and location.
+  //   var bounds = new google.maps.LatLngBounds();
+  //   places.forEach(function(place) {
+  //      if (!place.geometry) {
+  //        console.log("Returned place contains no geometry");
+  //        return;
+  //      }
+  //      var icon = {
+  //        url: place.icon,
+  //        size: new google.maps.Size(71, 71),
+  //        origin: new google.maps.Point(0, 0),
+  //        anchor: new google.maps.Point(17, 34),
+  //        scaledSize: new google.maps.Size(25, 25)
+  //      };
+  //
+  //      // Create a marker for each place.
+  //       brackMarkers.push(new google.maps.Marker({
+  //         map: map,
+  //         icon: icon,
+  //         title: place.name,
+  //         position: place.geometry.location
+  //       }));
+  //
+  //       if (place.geometry.viewport) {
+  //       // Only geocodes have viewport.
+  //         bounds.union(place.geometry.viewport);
+  //       } else {
+  //         bounds.extend(place.geometry.location);
+  //       }
+  //     })
+  //     map.fitBounds(bounds);
+  //   })
+  // })
   // add users current location and marker
   var userLatLng = new google.maps.LatLng(brackMap.lat, brackMap.lng);
   var userMarker = new google.maps.Marker({
